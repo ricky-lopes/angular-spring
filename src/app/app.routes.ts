2a85d77c08cc67b1fe201a67029967920431ea1f
Ricky <rickyblopes@gmail.com>
2024-07-17 20:47:16 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TesteComponent } from './pages/teste/teste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'teste', component: TesteComponent
+  }
+];
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+})
+export class NavbarComponent {
+
+
+}
